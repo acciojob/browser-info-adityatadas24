842f87291929423541b89ab75d81619e09f01057
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-25 13:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ const browserInfoDiv = document.getElementById("browser-info");
+
+        // Get the user agent string, browser name, and version from the navigator object
+        const userAgent = navigator.userAgent;
+        const browserName = navigator.appName;
+        const browserVersion = navigator.appVersion;
+
+        // Create a message with the browser information
+        const message = "You are using " + browserName + " version " + browserVersion;
+
+        // Display the message in the browser-info div
+        browserInfoDiv.textContent = message;
